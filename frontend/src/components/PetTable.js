ef20d1666d126b62923eda05a8edb4dad91db5d7
greyluo <luo80480@gmail.com>
2023-05-29 20:06:02 -0700

    fix

--- frontend/src/components/PetTable.js
+++ frontend/src/components/PetTable.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import { Typography } from '@mui/material';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Link, TextField, Box } from '@mui/material';
-import { useState } from 'react';
-// Assuming this is your data
-const data = [
-  { petName: 'Fido', ownerName: 'John Doe', petLink: '/fido', staffName: 'Staff 1' },
-  { petName: 'Spot', ownerName: 'Jane Doe', petLink: '/spot', staffName: 'Staff 2' },
-  // ... more data
-];
+import { useState, useEffect } from 'react';
 
-function PetTable() {
+// Assuming this is your data
+
+//
+function PetTable({token,setToken}) {
+    const handleLogout = () => {
+        setToken({token:null});
+      };
+
+    const [data, setData] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
+    useEffect(() => {
+        fetch('http://localhost:8080/getpetbyhospital', {
+            headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+            // Add any additional headers as needed
+            },
+        }).then(response => {
+            if (response.status === 401) {
+                handleLogout();
+              }
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+
+            else {return response.json();}
+          })
+          .then(petData => {
+            setData(petData)
+            console.log(petData)
+        })
+          .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+          });
+      }, []);
     const handleSearchChange = (event) => {
         setSearchTerm(event.target.value);
       };
       const filteredData = data.filter((item) =>
-      item.petName.toLowerCase().includes(searchTerm.toLowerCase()) || item.ownerName.toLowerCase().includes(searchTerm.toLowerCase())
+      item.name.toLowerCase().includes(searchTerm.toLowerCase()) || item.owner_name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     return (
@@ -62,12 +89,12 @@ function PetTable() {
                   <TableRow key={index}>
                     <TableCell component="th" scope="row">{index + 1}</TableCell>
                     <TableCell>
-                      <Link href={row.petLink} underline="hover" color="inherit">
-                        {row.petName}
+                      <Link href={`/petview/${row.id}`} underline="hover" color="inherit">
+                        {row.name}
                       </Link>
                     </TableCell>
-                    <TableCell>{row.ownerName}</TableCell>
-                    <TableCell>{row.staffName}</TableCell>
+                    <TableCell>{row.owner_name}</TableCell>
+                    <TableCell>{row.staff_name}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
