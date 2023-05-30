22c0ba72f6c1c81bd9d744cfffa46949111eb93a
greyluo <luo80480@gmail.com>
2023-05-29 20:11:19 -0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -90,11 +90,11 @@ function App() {
           <Toolbar />
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Routes>
-              <Route path="/dashboard" element={<Dashboard role={role}/>} />
+              <Route path="/dashboard" element={<Dashboard role={role} token={token} setToken={setToken}/>} />
               <Route path="/profile" element={<Profile setToken={setToken} token= {token} role={role} />} />
               {role==="pet owner" && <Route path="/addPet" element={<AddPet token = {token} />} />}
               <Route path= "/signup" element={<SignUp />} />
-              <Route path="/petview/:id" element={<PetView role={role}  />} />
+              <Route path="/petview/:id" element={<PetView role={role} token={token}  />} />
               <Route path="/editpet" element={<EditPet  />} />
               <Route path="/signup" element={<SignUp />} />
               {role==="hospital" && <Route path="/linkPet" element={<LinkPet token = {token} />} />}
